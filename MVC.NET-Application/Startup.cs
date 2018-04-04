b8e2534a5b95b0f8f71c7ade41a87a87e4be7b89
Jardine <jardine.miller@gmail.com>
2018-04-04 12:34:23 +0100

    restarted project

---
+++ MVC.NET-Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC.NET_Application.Startup))]
+namespace MVC.NET_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
